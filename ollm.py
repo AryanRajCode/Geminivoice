5c42249888eaa15a65a767c17fc2abc5cf34c935
Iam <168881081+AryanRajCode@users.noreply.github.com>
2024-05-27 13:34:39 +0530

    Update ollm.py

--- ollm.py
+++ ollm.py
@@ -1,14 +1,1 @@
-import ollama
 
-def olama(user, model):
-    while True:
-        response = ollama.chat(model, messages=[
-            {
-                'role': 'user',
-                'content': user,
-            },
-        ])
-        return response['message']['content']
-
-if __name__ == "__main__":
-    pass
